7b8cc5076124586e2cb602cd8b54711b60c531bb
DaniChinwendu <88294154+DaniChinwendu@users.noreply.github.com>
2023-01-30 00:35:06 +0100

    Update ewc_1_streamlit_py.py

--- ewc_1_streamlit_py.py
+++ ewc_1_streamlit_py.py
@@ -8,8 +8,6 @@ Original file is located at
 """
 
 # Commented out IPython magic to ensure Python compatibility.
-# 
-# %%writefile app.py
 # import numpy as np
 # from sklearn.preprocessing import MinMaxScaler
 # import pickle
